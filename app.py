af72d80db5b9c92279f529d1c8cc2bea1e49b9b0
fvessuri <fvessuri@gmail.com>
2021-09-01 20:34:40 -0300

    FV: Nuevas librerías en requirements.txt y form para opción Text Scrapping

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import eli5
 from eli5.lime import TextExplainer
 
+import praw
+import re
+from datetime import datetime
+import twint
+import nest_asyncio
+nest_asyncio.apply()
+
 def main():
     #   Con esto se configura un titulo e icono para la página web
     #   st.set_page_config(page_title="Grupo 6 - TP 4 App", page_icon="icon_g6_tp4.png", initial_sidebar_state="auto")
@@ -33,6 +40,14 @@ def main():
 def text_scrapping():
     st.subheader("Text scrapping")
     st.write("")
+    with st.form("frm_text_scrap"):
+        droga = st.text_input("Nombre de la droga: ")
+        busqueda = ["Twitter", "Reddit"]
+        busqueda_sel = st.selectbox("Opciones de búsqueda", busqueda)
+        submit_sel = st.submit_btn(label=" Iniciar búsqueda ")
+        if submit_sel:
+            st.success("Ha iniciado la búsqueda de la droga {} en {}", droga, busqueda_sel)
+
     st.write("")
 
 def file_uploading():
@@ -53,6 +68,42 @@ def about():
     st.write("Jorge Corro")
     st.write("Franco Visitini")
     st.write("Federico Vessuri")
+
+def buscar_tweets(droga):
+    #### BUSCO TWEETS QUE CONTENGAN LA FRASE "Droga is" y despues lo guardo en el DF Tweets_df
+    c = twint.Config()
+    Busqueda =   """\"""" + droga + " is" + """\""""
+    c.Search = Busqueda
+    c.Limit = 200
+    c.Pandas = True
+    c.Lang="en"
+    twint.run.Search(c)
+    Tweets_df = twint.storage.panda.Tweets_df
+    Tweets_df['droga'] = droga
+    Tweets_df=Tweets_df[['date','tweet','droga']]
+    Tweets_df.rename(columns={'tweet': 'review'}, inplace=True)
+    c.Hide_output = True
+    return Tweets_df
+
+def buscar_reddit(subredd , droga):
+    i=0
+    column_names = ["droga", "review", "date"]
+    df = pd.DataFrame(columns = column_names)
+    Subreddit = subredd ### el subreddit donde quiero hacer la busqueda
+    Busqueda =   """\"""" + droga + " is" + """\""""
+    reddit = get_reddit_credentials()
+    subR = reddit.subreddit(Subreddit)
+    resp = subR.search(Busqueda,limit=100)
+    for submission in resp:
+        df.at[i, 'droga'] = droga
+        df.at[i, 'review'] =str(str(submission.title.encode('ascii', 'ignore').decode("utf-8")) +" "+ str(submission.selftext[:120].encode('ascii', 'ignore').decode("utf-8")))        
+        df.at[i, 'date'] = datetime.utcfromtimestamp(int((submission.created_utc))).strftime('%Y-%m')
+        i+=1
+    return df
+
+def get_reddit_credentials():
+    return praw.Reddit(client_id='5U6IG9mVmOBz08m7gb_z8Q',client_secret='Y8yZhKAmDk6ryyEiXutrM0SVgnAMEg',username='jboirazian',password='+xj<_6$9hsZ7E)L',user_agent='jboirazian_grupo4')
+
 
 if __name__ == '__main__':
     main()
