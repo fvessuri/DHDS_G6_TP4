365713f3455dcd340e544d44ba47d7b92bd76d9b
fvessuri <fvessuri@gmail.com>
2021-09-02 22:47:50 -0300

    FV: Se agrega funcionalidad asociada a la opción de File Uploading de la app y se agregan un par de librerías a la lista de imports del archivo requirements.txt

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ import twint
 import nest_asyncio
 # nest_asyncio.apply()
 
+from nltk import RegexpTokenizer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
 def main():
     #   Con esto se configura un titulo e icono para la página web
     #   st.set_page_config(page_title="Grupo 6 - TP 4 App", page_icon="icon_g6_tp4.png", initial_sidebar_state="auto")
@@ -44,10 +50,17 @@ def text_scrapping():
         droga = st.text_input("Nombre de la droga: ")
         busqueda = ["Twitter", "Reddit"]
         busqueda_sel = st.selectbox("Opciones de búsqueda", busqueda)
+        subreddit = st.text_input("Nombre del subreddit: ")
         submit_sel = st.form_submit_button(label=" Iniciar búsqueda ")
         if submit_sel:
             if droga:
-                st.success("Ha iniciado la búsqueda de la droga '{}' en {}". format(droga, busqueda_sel))
+                if busqueda_sel == "Twitter":
+                    st.success("Ha iniciado la búsqueda de la droga '{}' en {}". format(droga, busqueda_sel))
+                else:
+                    if subreddit:
+                        st.success("Ha iniciado la búsqueda de la droga '{}' en {}". format(droga, busqueda_sel))
+                    else:
+                        st.warning("Debe seleccionar un subreddit para realizar la búsqueda")
             else:
                 st.warning("Debe seleccionar una droga para realizar la búsqueda")
 
@@ -57,11 +70,13 @@ def file_uploading():
     st.subheader("File uploading")
     st.write("")
     with st.form("frm_file_upload"):
-        file_up = st.file_uploader("Seleccione un archivo (csv) para procesar: ", type='csv')
+        st.write("El archivo debe ser formato csv y debe contener al menos las siguientes columnas: date, review, droga")
+        file_upr = st.file_uploader("Seleccione un archivo (csv) para procesar: ", type='csv')
         submit_fil = st.form_submit_button(label=" Iniciar proceso ")
         if submit_fil:
-            if file_up:
+            if file_upr:
                 st.success("Ha iniciado el proceso del archivo seleccionado")
+                procesar_archivo(file_upr)
             else:
                 st.warning("Debe seleccionar un archivo antes de iniciar el proceso")
     st.write("")
@@ -85,7 +100,7 @@ def about():
     st.write("Mariana Peinado")
     st.write("Juan Boirazian")
     st.write("Jorge Corro")
-    st.write("Franco Visitini")
+    st.write("Franco Visintini")
     st.write("Federico Vessuri")
 
 def buscar_tweets(droga):
@@ -123,6 +138,75 @@ def buscar_reddit(subredd , droga):
 def get_reddit_credentials():
     return praw.Reddit(client_id='5U6IG9mVmOBz08m7gb_z8Q',client_secret='Y8yZhKAmDk6ryyEiXutrM0SVgnAMEg',username='jboirazian',password='+xj<_6$9hsZ7E)L',user_agent='jboirazian_grupo4')
 
+def limpiar_texto(texto):
+    # poner todo en minúscula
+    minuscula = texto.lower()   
+    # quitar patrón de repetición
+    pattern=minuscula.replace('&#039;', "'")
+    # Remover caracteres especiales
+    special_remove = pattern.replace(r'[^\w\d\s]',' ')    
+    # Remover los caracteres no ASCII
+    ascii_remove = special_remove.replace(r'[^\x00-\x7F]+',' ')    
+    # Remover espacios en blanco iniciales y finales
+    whitespace_remove = ascii_remove.replace(r'^\s+|\s+?$','')    
+    # Replazar multiples espacios en blanco con un ùnico espacio
+    multiw_remove = whitespace_remove.replace(r'\s+',' ')    
+    # Replazar 2 o más puntos por 1 solo punto
+    dots_remove = multiw_remove.replace(r'\.{2,}', ' ')    
+    return dots_remove
+
+def contar_palabras(s):
+    return (len(s.split()))
+
+# Esta función aplica un tokenizador, genera stems y borra stop_words
+def clean_datos(review_text, tokenizer, stemmer, stopwords):    
+    stopwords_stem = [stemmer.stem(x) for x in stopwords]    #tokens (eliminamos todos los signos de puntuación)
+    words = tokenizer.tokenize(review_text)
+    # stemming: raiz y minúsculas:
+    stem_words = [stemmer.stem(x) for x in words]
+    # eliminamos stopwords (ya pasaron por stem)
+    clean_words = [x for x in stem_words if x not in stopwords_stem]
+    result = " ".join(clean_words)
+    return(result)
+
+def procesar_archivo(arch):
+    if arch is not None:
+        df = pd.read_csv(arch)
+        if df is not None:
+            st.write("dataframe del archivo:", df.head(5))
+            st.write("shape: ", df.shape)
+            procesar_dataframe(df)
+        else:
+            st.warning("No hay datos en el archivo para procesar")
+    else:
+        return None
+
+def procesar_dataframe(df):
+    # Se limpian las reviews del dataframe
+    st.write("Se limpian las reviews del dataframe...")
+    df.review.apply(limpiar_texto)
+    # Definimos tokenizador, stemmer y stop_words que utilizaremos en la función "clean_datos"
+    tokenizer = RegexpTokenizer(r"\w+")
+    englishStemmer = SnowballStemmer("english")
+    stopwords_en = stopwords.words('english')
+    # Se quitan las stopwords y se stemizan las palabras limpias de las reviews del dataframe
+    st.write("Se quitan las stopwords y se stemizan las palabras limpias de las reviews del dataframe...")
+    clean_review = [clean_datos(x, tokenizer, englishStemmer, stopwords_en) for x in df.review]
+    st.write("clean_review: ", clean_review)
+    # se vectorizan las reviews del dataframe
+    st.write("se vectorizan las reviews del dataframe...")
+    count_vectorizer = CountVectorizer(ngram_range=(1, 2), stop_words=stopwords_en)
+    # se genera la matriz esparsa
+    st.write("se genera la matriz esparsa...")
+    clean_review_sparse = count_vectorizer.transform(clean_review)
+    # se le aplica una transformación con tfidf
+    st.write("se le aplica una transformación con tfidf...")
+    tfidf_transformer = TfidfTransformer(sublinear_tf=True,  norm='l2')
+    clean_review_tfidf = tfidf_transformer.transform(clean_review_sparse)
+    # se le aplica una transfomación con SVD
+    st.write("se le aplica una transfomación con SVD...")
+    svd = TruncatedSVD(n_components = 200)
+    clean_review_svd = svd.transform(clean_review_tfidf)
 
 if __name__ == '__main__':
     main()
